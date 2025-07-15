09b7b7c83c2afa39a719ee020a733ebda0adc65c
omkar garde <42486951+omkargarde@users.noreply.github.com>
2025-07-15 15:43:01 +0530

    fix: adjust layout styles and structure in RootLayout and Home components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,21 @@ export default function Home() {
   const profiles: TTalentProfiles = TalentProfiles;
   return (
     <>
-      <h1 className="md:text-2xl text-3xl">Talent match maker</h1>
-      <ProfileGrid profiles={profiles} />
+      <h1 className="text-3xl md:text-2xl">Talent match maker</h1>
+      <div className="flex">
+        <section>
+          <section>
+            <h2>Location:</h2>
+          </section>
+          <section>
+            <h2>Skills:</h2>
+          </section>
+          <section>
+            <h2>Platform:</h2>
+          </section>
+        </section>
+        <ProfileGrid profiles={profiles} />
+      </div>
     </>
   );
 }
