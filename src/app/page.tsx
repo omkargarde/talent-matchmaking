21e15059dee584358d4f04e8598ff0016e7d3281
omkar garde <42486951+omkargarde@users.noreply.github.com>
2025-07-17 16:16:11 +0530

    feat: restructure Home component to include location, skills, and platform sections with improved layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,9 @@
-import { TalentProfiles } from "@/data/talent-profile";
+import {
+  TalentLocations,
+  TalentPlatforms,
+  TalentProfiles,
+  TalentSkills,
+} from "@/data/talent-profile";
 import { TTalentProfiles } from "@/types/Talent-profile";
 import ProfileGrid from "./(profile)/ProfileGrid";
 
@@ -7,18 +12,35 @@ export default function Home() {
   return (
     <>
       <h1 className="text-3xl md:text-2xl">Talent match maker</h1>
-      <div className="flex">
-        <section>
-          <section>
-            <h2>Location:</h2>
+      <div className="mx-4 flex gap-2">
+        <div>
+          <section className="rounded-2xl border p-4">
+            <section>
+              <h2 className="py-2 text-xl">Location:</h2>
+              <select>
+                {TalentLocations.map((location) => (
+                  <option key={location}>{location}</option>
+                ))}
+              </select>
+            </section>
+            <section>
+              <h2 className="py-2 text-xl">Skills:</h2>
+              <ul>
+                {TalentSkills.map((skill) => (
+                  <li key={skill}>{skill}</li>
+                ))}
+              </ul>
+            </section>
+            <section>
+              <h2 className="py-2 text-xl">Platform:</h2>
+              <ul>
+                {TalentPlatforms.map((Platform) => (
+                  <li key={Platform}>{Platform}</li>
+                ))}
+              </ul>
+            </section>
           </section>
-          <section>
-            <h2>Skills:</h2>
-          </section>
-          <section>
-            <h2>Platform:</h2>
-          </section>
-        </section>
+        </div>
         <ProfileGrid profiles={profiles} />
       </div>
     </>
